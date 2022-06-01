b671932ac63b8f04966bbe5e4ebcd5471a4aa1b5
Zinsou théagnorax GANDAHO <87780769+zinsou12@users.noreply.github.com>
2022-06-01 11:45:22 +0100

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,28 +1,12 @@
 <?php
-/* chargement des fichiers necessaires
 
-scan du 
-
-*/
-
-foreach(scandir('../functions') as $fichier)
+foreach(scandir(__DIR__.'/../functions') as $fichier)
 {
-    if(preg_match("#/.php$#", $fichier))
+    
+    if(preg_match("#(.php)$#", $fichier))
     {
         require __DIR__.'/../functions/'.$fichier;
     }
-    
+   
 }
 
-
-//var_dump(require_once __DIR__.'/../functions');
-
-
-
-
-
- //var_dump($_GET);die;
- /*
- affichage de la vue correspondante
- */
-echo run($_SERVER['REQUEST_METHOD']);
